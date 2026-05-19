3afab7293abf8a4f0139f2e1a583d2d036747ed7
Leo141592 <her25041@uvg.edu.gt>
2026-05-19 14:19:14 -0600

    Primera versión del Main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,17 @@
 package main;
 
 import model.Graph;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Graph graph = new Graph(20);
+
+        graph.addEdge("Mixco", "Antigua", 30);
+        graph.addEdge("Antigua", "Escuintla", 25);
+        graph.addEdge("Escuintla", "SantaLucia", 15);
+
+        graph.printMatrix();
+    }
+}
