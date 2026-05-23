bacd18bded3fb4155c5a3d95fc2e1960648860e3
Leo141592 <her25041@uvg.edu.gt>
2026-05-22 19:54:55 -0600

    Tests 1 2 y 3

--- src/test/GraphTest.java
+++ src/test/GraphTest.java
@@ -9,16 +9,62 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class GraphTest {
 
-}
+    @Test
+    public void testAddEdge() {
 
-@Test
-public void testAddEdge() {
+        Graph graph = new Graph(10);
 
-    Graph graph = new Graph(10);
+        graph.addEdge("Mixco", "Antigua", 30);
 
-    graph.addEdge("Mixco", "Antigua", 30);
+        double[][] matrix = graph.getAdjacencyMatrix();
 
-    double[][] matrix = graph.getAdjacencyMatrix();
+        assertEquals(30, matrix[0][1]);
+    }
 
-    assertEquals(30, matrix[0][1]);
+    @Test
+    public void testRemoveEdge() {
+
+        Graph graph = new Graph(10);
+
+        graph.addEdge("Mixco", "Antigua", 30);
+
+        graph.removeEdge("Mixco", "Antigua");
+
+        double[][] matrix = graph.getAdjacencyMatrix();
+
+        assertEquals(
+                Double.POSITIVE_INFINITY,
+                matrix[0][1]);
+    }
+
+    @Test
+    public void testFloyd() {
+
+        Graph graph = new Graph(10);
+
+        graph.addEdge("A", "B", 5);
+        graph.addEdge("B", "C", 3);
+
+        FloydResult result = graph.floyd();
+
+        double[][] dist = result.getDistances();
+
+        assertEquals(8, dist[0][2]);
+    }
+
+    @Test
+    public void testGraphCenter() {
+
+        Graph graph = new Graph(10);
+
+        graph.addEdge("A", "B", 2);
+        graph.addEdge("B", "C", 2);
+        graph.addEdge("A", "C", 10);
+
+        FloydResult result = graph.floyd();
+
+        String center = graph.getGraphCenter(result);
+
+        assertEquals("B", center);
+    }
 }
