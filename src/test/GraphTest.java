d70d6f328a4c1eda287615211ddb711e94f4c37d
Leo141592 <her25041@uvg.edu.gt>
2026-05-22 19:39:27 -0600

    Tests

--- src/test/GraphTest.java
+++ src/test/GraphTest.java
@@ -1,5 +1,24 @@
 package test;
 
+import model.Graph;
+import model.FloydResult;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GraphTest {
 
+}
+
+@Test
+public void testAddEdge() {
+
+    Graph graph = new Graph(10);
+
+    graph.addEdge("Mixco", "Antigua", 30);
+
+    double[][] matrix = graph.getAdjacencyMatrix();
+
+    assertEquals(30, matrix[0][1]);
 }
